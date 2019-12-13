60aa10d7bf8393aca5aa6fd1309726c8335136c7
Joseph Brown <joseph.d.brown94@gmail.com>
2019-12-13 11:28:10 -0500

    Added code to project to make it run properly. It should return required results.

--- tuneup.py
+++ tuneup.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
-__author__ = "???"
+__author__ = "Joey Brown with help from Coaches"
 
 import cProfile
 import pstats
 import functools
+import timeit
 
 
 def profile(func):
@@ -14,7 +15,17 @@ def profile(func):
     # You need to understand how decorators are constructed and used.
     # Be sure to review the lesson material on decorators, they are used
     # extensively in Django and Flask.
-    raise NotImplementedError("Complete this decorator function")
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        c = cProfile.Profile()
+        c.enable()
+        result = func(*args, **kwargs)
+        c.disable()
+        sort_by = 'cumulative'
+        cs = pstats.Stats(c).sort_stats(sort_by)
+        cs.print_stats()
+        return result
+    return inner
 
 
 def read_movies(src):
@@ -31,21 +42,29 @@ def is_duplicate(title, movies):
             return True
     return False
 
-
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
+    movie_dict = {}
     duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
+    for movie in movies:
+        if movie not in movie_dict:
+            movie_dict[movie] = movie
+        else:
             duplicates.append(movie)
     return duplicates
 
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
-    # YOUR CODE GOES HERE
+    t = timeit.Timer(stmt='find_duplicate_movies("movies.txt")',
+                     setup='from __main__ import find_duplicate_movies')
+    runs_per_call = 3
+    repeats = 7
+    result = t.repeat(repeat=repeats, number=runs_per_call)
+    print(min(result) / float(runs_per_call))
+                     
 
 
 def main():
